6c2181a84a87aa876b24a6cf0daa905c298e603f
BerkeDüzgün <BerkeDzgn@BERKE>
2022-08-28 01:24:51 +0300

    Add project files.

---
+++ src/Projections/UniSozluk.Projections.FavoriteService/Program.cs
@@ -0,0 +1,10 @@
+using UniSozluk.Projections.FavoriteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
