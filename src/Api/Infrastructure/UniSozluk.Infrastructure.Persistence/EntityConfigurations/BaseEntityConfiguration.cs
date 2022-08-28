6a0b997f0fa8b19986b0e08be3d1842f1a493103
Berke Düzgün <duzgunberke@gmail.com>
2022-08-28 17:14:00 +0300

    context configuration

---
+++ src/Api/Infrastructure/UniSozluk.Infrastructure.Persistence/EntityConfigurations/BaseEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UniSozluk.Api.Domain.Models;
+
+namespace UniSozluk.Infrastructure.Persistence.EntityConfigurations
+{
+    public class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Id).ValueGeneratedOnAdd();
+            builder.Property(i=>i.CreateDate).ValueGeneratedOnAdd();
+        }
+    }
+}
